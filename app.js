fbae08671bb4de85bba87c06dfeae86d09656307
Daniel Bemsen <danireptor7@gmail.com>
2020-07-07 23:53:39 +0100

    Improvement on facebook's OAuth feature. 70% done

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
@@ -72,6 +73,18 @@ passport.use(new GoogleStrategy({
   }
 ));
 
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 app.get( '/', (req, res) => {
         res.render('home')
     });
@@ -87,9 +100,17 @@ app.get('/auth/google/secrets',
     res.redirect('/secrets');
 });
 
+
 app.get('/auth/facebook',
-    
-);
+  passport.authenticate('facebook', { scope: ['profile'] }));
+
+app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
+
 
 app.get( '/login', (req, res) => {
         res.render('login')
